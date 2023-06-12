2d322d98b53a97a96cfe70baeefddf510521c210
Jonatandb <jonatandb@gmail.com>
2023-06-12 20:04:32 -0300

    Password encryptation using Bcryptjs module

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,24 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
   const { username, password, email } = req.body
 
   try {
-    const newUser = new User({ username, password, email })
+    const passwordHash = await bcrypt.hash(password, 10)
+    const newUser = new User({
+      username,
+      password: passwordHash,
+      email
+    })
     const userSaved = await newUser.save()
-    res.send(userSaved)
+    res.send({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    })
   } catch (error) {
     console.log(error)
     res.sendStatus(400)
