2cd71884668c4073ce133755d583f357de3e8865
Jonatandb <jonatandb@gmail.com>
2023-06-14 12:00:16 -0300

    Tasks routes and tasks CRUD added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,26 @@
-import mongoose, { model } from "mongoose";
+import mongoose, { model } from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
+  {
+    timestamps: true,
   },
-  password: {
-    type: String,
-    required: true,
-  }
-}, {
-  timestamps: true
-})
+)
 
-export default model("User", userSchema)
+export default model('User', userSchema)
